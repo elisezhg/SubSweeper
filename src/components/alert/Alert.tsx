9fe899b1cd55031f167d5ea5770046bfe6797689
elisezhg <elise.zheng@hotmail.fr>
2024-02-20 23:55:21 -0500

    fix alert close btn focus ♿️

--- src/components/alert/Alert.tsx
+++ src/components/alert/Alert.tsx
@@ -1,12 +1,20 @@
 import './Alert.scss';
 
+export type AlertType = 'success' | 'error';
+
 interface AlertProps {
-  type: 'success' | 'error';
+  type: AlertType;
   children: React.ReactNode;
+  onClose: () => void;
 }
 
 export default function Alert(props: AlertProps) {
-  const { type, children } = props;
+  const { type, onClose, children } = props;
 
-  return <div className={`alert alert--${type}`}>{children}</div>;
+  return (
+    <div className={`alert alert--${type}`}>
+      <span className='alert__close-btn' onClick={onClose} tabIndex={0} />
+      {children}
+    </div>
+  );
 }
